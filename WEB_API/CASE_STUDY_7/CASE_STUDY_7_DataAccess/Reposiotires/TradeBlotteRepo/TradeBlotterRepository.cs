80313c8983bf8ffd1d70a5b74c30f3ca57b91c86
Tejas <tdgunjal@ivp.in>
2026-08-06 23:55:34 +0530

    Implement TradeBlotterRepository and Interface

---
+++ WEB_API/CASE_STUDY_7/CASE_STUDY_7_DataAccess/Reposiotires/TradeBlotteRepo/TradeBlotterRepository.cs
@@ -0,0 +1,70 @@
+﻿using CASE_STUDY_7.DataAccess;
+using CASE_STUDY_7_Models.DTOs;
+using CASE_STUDY_7_Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CASE_STUDY_7_DataAccess.Reposiotires.TradeBlotteRepo
+{
+    public class TradeBlotterRepository : ITradeBlotterRepository
+    {
+        private readonly Vantage7Context _context;
+
+        public TradeBlotterRepository(Vantage7Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<TradeBlotterPagedResultDto> GetTradeBlotterAsync(
+            TradeBlotterRequestDto request,
+            CancellationToken cancellationToken = default)
+        {
+            var query = _context.VwTradeBlotters.AsNoTracking().AsQueryable();
+
+            if (request.FromDate.HasValue)
+            {
+                query = query.Where(x => x.TradeDate >= request.FromDate.Value);
+            }
+
+            if (request.ToDate.HasValue)
+            {
+                query = query.Where(x => x.TradeDate <= request.ToDate.Value);
+            }
+
+            var totalCount = await query.CountAsync(cancellationToken);
+
+            // 4. Paginate and project directly into TradeBlotterItemDto
+            var items = await query
+                .OrderByDescending(x => x.TradeDate)
+                .ThenByDescending(x => x.TradeId)
+                .Skip((request.PageNumber - 1) * request.PageSize)
+                .Take(request.PageSize)
+                .Select(x => new TradeBlotterItemDto
+                {
+                    TradeId = x.TradeId,
+                    TradeDate = x.TradeDate,
+                    SecurityId = x.SecurityId,
+                    SecurityName = x.SecurityName,
+                    TraderId = x.TraderId,
+                    TraderName = x.TraderName,
+                    BuySell = x.BuySell,
+                    Quantity = x.Quantity,
+                    Price = x.Price,
+                    GrossNotionalAmount = x.GrossNotionalAmount ?? 0m // Replaces null with 0.0
+                })
+                .ToListAsync(cancellationToken);
+
+            return new TradeBlotterPagedResultDto
+            {
+                TotalRecords = totalCount,
+                PageNumber = request.PageNumber,
+                PageSize = request.PageSize,
+                Items = items
+            };
+        }
+    }
+}
